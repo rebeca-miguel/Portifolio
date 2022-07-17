9694a7fa6d7a48d5de81c1807d061ce7a435cc1c
rebeca-miguel <rebecagaspar19@gmail.com>
2022-07-17 13:43:20 -0300

    Portifolio

---
+++ Javascript/script.js
@@ -0,0 +1,4 @@
+const nomedoUsuario = prompt("qual seu nome?");
+const ano = prompt("que ano você nasceu");
+const SexoBiologico = prompt("qual seu sexo biológico? por favor indicar uma das opções: 'femenino' ou 'masculino');
+const contribuição = prompt("quantos anos você tem contrimuindo com o INSS?");
